bda34a4d7ee090487ac2ce5a113a62243065fbac
aayushgour <ag14906@gmail.com>
2024-01-16 10:09:40 +0530

    updated pdfmake version, added config for ttf files, bug fix in edit estimate component, modified invoice component, attendance component rewritten, modified pdf preview component

--- src/components/utility/pdf-preview.jsx
+++ src/components/utility/pdf-preview.jsx
@@ -18,72 +18,86 @@ function PdfPreview({ docDefinition, className }) {
 
     const [pdfUrl, setPdfUrl] = useState(null);
 
-    useEffect(() => {
-        const generatePdf = async () => {
-            pdfMake.vfs = pdfFonts && pdfFonts.pdfMake ? pdfFonts.pdfMake.vfs : pdfMake.vfs;
+    const generatePdf = async () => {
+        pdfMake.vfs = pdfFonts && pdfFonts.pdfMake ? pdfFonts.pdfMake.vfs : pdfMake.vfs;
 
-            pdfMake.vfs["Helvetica.ttf"] = await convertImageToBase64(HelveticaFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
-            pdfMake.vfs["Helvetica-Bold.ttf"] = await convertImageToBase64(HelveticaBoldFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
-            pdfMake.vfs["Helvetica-Oblique.ttf"] = await convertImageToBase64(HelveticaItalicFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
-            pdfMake.vfs["Helvetica-BoldOblique.ttf"] = await convertImageToBase64(HelveticaBoldItalicFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
-            pdfMake.vfs["Kannada-font.ttf"] = await convertImageToBase64(KannadaFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
-            pdfMake.vfs["Times-New-Roman.ttf"] = await convertImageToBase64(TimesNewRomanFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
-            pdfMake.vfs["Times-New-Roman-Bold.ttf"] = await convertImageToBase64(TimesNewRomanBoldFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
+        const helveticaFont = new URL("../../assets/fonts/Helvetica.ttf", import.meta.url).href;
+        const helveticaBoldFont = new URL("../../assets/fonts/Helvetica-Bold.ttf", import.meta.url).href;
+        const helveticaItalicFont = new URL("../../assets/fonts/Helvetica-Oblique.ttf", import.meta.url).href;
+        const helveticaBoldItalicFont = new URL("../../assets/fonts/Helvetica-BoldOblique.ttf", import.meta.url).href;
+        const kannadaFont = new URL("../../assets/fonts/Kannada-font.ttf", import.meta.url).href;
+        const timesNewRomanFont = new URL("../../assets/fonts/TIMES.ttf", import.meta.url).href;
+        const timesNewRomanBoldFont = new URL("../../assets/fonts/TIMESBD.ttf", import.meta.url).href;
 
-            const docDef = {
-                ...docDefinition,
-                defaultStyle: {
-                    font: 'Helvetica',
-                    lineHeight: 1.2,
-                }
+        // pdfMake.vfs["Helvetica.ttf"] = await convertImageToBase64(HelveticaFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
+        // pdfMake.vfs["Helvetica-Bold.ttf"] = await convertImageToBase64(HelveticaBoldFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
+        // pdfMake.vfs["Helvetica-Oblique.ttf"] = await convertImageToBase64(HelveticaItalicFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
+        // pdfMake.vfs["Helvetica-BoldOblique.ttf"] = await convertImageToBase64(HelveticaBoldItalicFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
+        // pdfMake.vfs["Kannada-font.ttf"] = await convertImageToBase64(KannadaFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
+        // pdfMake.vfs["Times-New-Roman.ttf"] = await convertImageToBase64(TimesNewRomanFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
+        // pdfMake.vfs["Times-New-Roman-Bold.ttf"] = await convertImageToBase64(TimesNewRomanBoldFont)?.then(res => res?.replace("data:font/ttf;base64,", ""));
+
+        const docDef = {
+            ...docDefinition,
+            defaultStyle: {
+                font: 'Helvetica',
+                lineHeight: 1.2,
             }
-            pdfMake.fonts = {
-                ...pdfMake.fonts,
-                // Courier: {
-                //     normal: 'Courier',
-                //     bold: 'Courier-Bold',
-                //     italics: 'Courier-Oblique',
-                //     bolditalics: 'Courier-BoldOblique'
-                // },
-                Helvetica: {
-                    normal: "Helvetica.ttf",
-                    bold: 'Helvetica-Bold.ttf',
-                    italics: 'Helvetica-Oblique.ttf',
-                    bolditalics: 'Helvetica-BoldOblique.ttf'
-                },
-                "Kannada-font": {
-                    normal: "Kannada-font.ttf",
-                    bold: 'Kannada-font.ttf',
-                    italics: 'Kannada-font.ttf',
-                    bolditalics: 'Kannada-font.ttf'
-                },
-                "Times-New-Roman": {
-                    normal: 'Times-New-Roman.ttf',
-                    bold: 'Times-New-Roman-Bold.ttf',
-                    italics: 'Times-New-Roman.ttf',
-                    bolditalics: 'Times-New-Roman.ttf'
-                },
-                // Symbol: {
-                //     normal: 'Symbol'
-                // },
-                // ZapfDingbats: {
-                //     normal: 'ZapfDingbats'
-                // }
-            };
-
-            // pdfMake.vfs["Helvetica"] = await convertImageToBase64(HelveticaFont);
-            // pdfMake.vfs["Helvetica"] = await convertImageToBase64(HelveticaFont);
-            const pdfDocGenerator = pdfMake.createPdf(docDef, {}, pdfMake.fonts);
-
-            const pdfData = await new Promise((resolve, reject) => {
-                pdfDocGenerator.getBase64((data) => {
-                    resolve(data);
-                });
-            });
-
-            setPdfUrl(pdfData);
+        }
+        const fonts = {
+            ...pdfMake.fonts,
+            // Courier: {
+            //     normal: 'Courier',
+            //     bold: 'Courier-Bold',
+            //     italics: 'Courier-Oblique',
+            //     bolditalics: 'Courier-BoldOblique'
+            // },
+            // Helvetica: {
+            //     normal: "Helvetica.ttf",
+            //     bold: 'Helvetica-Bold.ttf',
+            //     italics: 'Helvetica-Oblique.ttf',
+            //     bolditalics: 'Helvetica-BoldOblique.ttf'
+            // },
+            Helvetica: {
+                normal: helveticaFont,
+                bold: helveticaBoldFont,
+                italics: helveticaItalicFont,
+                bolditalics: helveticaBoldItalicFont,
+            },
+            "Kannada-font": {
+                normal: kannadaFont,
+                bold: kannadaFont,
+                italics: kannadaFont,
+                bolditalics: kannadaFont,
+            },
+            "Times-New-Roman": {
+                normal: timesNewRomanFont,
+                bold: timesNewRomanBoldFont,
+                italics: timesNewRomanFont,
+                bolditalics: timesNewRomanFont
+            },
+            // Symbol: {
+            //     normal: 'Symbol'
+            // },
+            // ZapfDingbats: {
+            //     normal: 'ZapfDingbats'
+            // }
         };
 
+        // pdfMake.vfs["Helvetica"] = await convertImageToBase64(HelveticaFont);
+        // pdfMake.vfs["Helvetica"] = await convertImageToBase64(HelveticaFont);
+        const pdfDocGenerator = pdfMake.createPdf(docDef, {}, fonts);
+
+        const pdfData = await new Promise((resolve, reject) => {
+            pdfDocGenerator.getBase64((data) => {
+                resolve(data);
+            });
+        });
+
+        setPdfUrl(pdfData);
+    };
+
+    useEffect(() => {
         generatePdf();
     }, [docDefinition]);
 
